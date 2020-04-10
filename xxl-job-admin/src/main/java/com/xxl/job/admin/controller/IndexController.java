b43371bf7a691a973f2ae6942307f5f6b4e6d06f
liugk <liugk@lizi.com>
2020-04-10 11:50:10 +0800

    sss

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/IndexController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/IndexController.java
@@ -37,7 +37,7 @@ public class IndexController {
 
 		Map<String, Object> dashboardMap = xxlJobService.dashboardInfo();
 		model.addAllAttributes(dashboardMap);
-		System.out.println("aa");
+		System.out.println("bb");
 		return "index";
 	}
 
